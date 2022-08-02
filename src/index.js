a4cdd9e4f48b7d00718921fe73b00cef488b546c
abbas-77 <abbas.azimii77@gmail.com>
2022-08-02 13:28:07 +0430

    by

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const client = new ApolloClient({
   version: "1.0",
 });
 
-console.log("<><><><><>", InMemoryCache());
+console.log("<><><><><>", client);
 
 ReactDOM.render(
   <ApolloProvider client={client}>
